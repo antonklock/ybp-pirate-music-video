324dad521aadfad482b07313305c9f07dab61119
Anton Klock <klock.anton@gmail.com>
2024-03-09 21:24:15 +0100

    adding pixiJs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+	import { pixiJsMain } from '$lib/components/pixijs/pixiMain.js';
+	export let data;
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		pixiJsMain();
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ul>
+	{#each data.countries as country}
+		<li>{country.name}</li>
+	{/each}
+</ul>
