1b112db777c96decb38584fa9e49366952853a9c
Anton Klock <klock.anton@gmail.com>
2024-04-13 23:37:22 +0200

    making DASH work with seamless video switching and starting to overlay PIXI for graphics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,93 +1,167 @@
 <script lang="ts">
-	export let data;
-	import { pixiJsMain, playCurrentVideo, setCurrentVideo } from '$lib/components/pixijs/pixiMain';
 	import { onMount } from 'svelte';
-	import { videos } from '$lib/game/videoConfig';
-	import PixiSound from '$lib/components/PixiSound.svelte';
 
-	let isHidden = true;
+	import { Application, Assets, Sprite, Texture } from 'pixi.js';
 
-	const startVideo = () => {
-		const newVideoObject = getVideoObjectByName('Hub');
-		setCurrentVideo(newVideoObject);
+	const videoUrls = [
+		'https://customer-8b2ok7c97mpbuf67.cloudflarestream.com/e7634c71f936fe50eb555a45f1263548/manifest/video.mpd',
+		'https://customer-8b2ok7c97mpbuf67.cloudflarestream.com/aec3884a2d861238267ef6df82680c90/manifest/video.mpd',
+		'https://customer-8b2ok7c97mpbuf67.cloudflarestream.com/79b80511e39039c9c99b127f6f515e0e/manifest/video.mpd',
+		'https://customer-8b2ok7c97mpbuf67.cloudflarestream.com/0bbec7b68c56905e6b10959ce15d91e3/manifest/video.mpd'
+	];
 
-		videos.forEach((video) => {
-			video.videoSprite.texture.source.resource.play();
-			video.videoSprite.texture.source.resource.pause();
-		});
+	let currentVideoIndex = 0;
+	let videoPlayers: dashjs.MediaPlayerClass[] = [];
+	let videoElements: HTMLVideoElement[] = [];
+	let mainVideoElement: HTMLVideoElement | null = null;
 
-		setTimeout(() => {
-			playCurrentVideo();
+	let hiddenCanvas: HTMLCanvasElement;
+	let canvasContext: CanvasRenderingContext2D | null;
 
-			isHidden = !isHidden;
-		}, 10);
-	};
+	let videoTexture: Texture;
+	let videoSprite: Sprite;
+
+	function updateTexture() {
+		canvasContext?.drawImage(videoElements[currentVideoIndex], 0, 0);
+		videoTexture = Texture.from(hiddenCanvas);
+		videoSprite = Sprite.from(videoTexture);
+
+		videoSprite.width = 800 / 2;
+		videoSprite.height = 450 / 2;
+		videoSprite.x = 800 / 2;
+		videoSprite.y = 450 / 2;
+
+		videoSprite.anchor.set(0.5);
+	}
 
 	onMount(async () => {
-		const pixiElement = document.getElementById('pixiElement');
-		if (pixiElement) pixiJsMain(pixiElement);
+		hiddenCanvas = document.createElement('canvas');
+		hiddenCanvas.width = 800;
+		hiddenCanvas.height = 450;
+		canvasContext = hiddenCanvas.getContext('2d');
 
-		const videoPlayer = window.document.getElementById('video');
-	});
+		mainVideoElement = document.querySelector('.video-container');
+		const pixiDiv = document.querySelector('#pixi') as HTMLDivElement;
+		pixiDiv.style.position = 'absolute';
 
-	const getVideoObjectByName = (name: string) => {
-		const videoObject = videos.filter((videoObject) => {
-			if (videoObject.name === name) return videoObject;
+		const app = new Application();
+		await app.init({ width: 800, height: 450, backgroundAlpha: 0.25 });
+		app.renderer.background.color = [0, 0, 0, 0.25];
+
+		pixiDiv?.appendChild(app.canvas);
+		if (pixiDiv) pixiDiv.style.zIndex = '100';
+
+		const pirateTexture = await Assets.load<Texture>('/images/pirate.png');
+		const pirateSprite = new Sprite(pirateTexture);
+
+		pirateSprite.scale.set(0.5);
+		pirateSprite.anchor.set(0.5);
+		pirateSprite.y = app.screen.height / 2;
+
+		app.stage.addChild(pirateSprite);
+
+		app.ticker.add(() => {
+			pirateSprite.x += 2;
+			pirateSprite.rotation += Math.sin(pirateSprite.x / 3) * 0.02;
+			if (pirateSprite.x > app.screen.width) {
+				pirateSprite.x = -pirateSprite.width;
+			}
+
+			if (videoSprite) {
+				app.stage.children.includes(videoSprite) || app.stage.addChild(videoSprite);
+
+				updateTexture();
+			}
 		});
 
-		return videoObject[0];
-	};
+		// if (true) {
+		// 	console.log('Skipping dashjs');
+		// 	return;
+		// }
+		const dashjs = await import('dashjs');
+
+		const videosDiv = document.querySelector('#videos') as HTMLDivElement;
+
+		for (const url of videoUrls) {
+			const player = dashjs.MediaPlayer().create();
+
+			const videoElement = document.createElement('video');
+			videoElement.style.position = 'absolute';
+			videoElement.preload = 'metadata';
+			videoElement.style.display = 'none';
+			videoElement.style.width = '800px';
+			videoElement.style.height = '450px';
+			videoElement.style.borderRadius = '5px';
+
+			if (mainVideoElement) mainVideoElement.appendChild(videoElement);
+
+			player.initialize(videoElement, url, false);
+			player.setMute(true);
+
+			videoElements.push(videoElement);
+			videoPlayers.push(player);
+		}
+
+		console.log(videoPlayers);
+
+		videoElements[0].style.display = 'block';
+	});
+
+	function switchVideo() {
+		currentVideoIndex = (currentVideoIndex + 1) % videoUrls.length;
+
+		for (let i = 0; i < videoPlayers.length; i++) {
+			videoPlayers[i].seek(0);
+			videoPlayers[i].pause();
+			videoElements[i].style.display = 'none';
+		}
+
+		const player = videoPlayers[currentVideoIndex];
+		const element = videoElements[currentVideoIndex];
+
+		element.style.display = 'block';
+		player.play();
+	}
 </script>
 
-<h1>Welcome to Pirate Music Video</h1>
-<p>This is a testing ground for our new project</p>
+<div class="container">
+	<h1>Welcome to Pirate Music Video</h1>
+	<p>This is a testing ground for our new project</p>
 
-<ul>
-	{#each data.countries as country}
-		<li>{country.name}</li>
-	{/each}
-</ul>
+	<div class="video-container">
+		<div id="pixi"></div>
+		<div id="videos"></div>
+	</div>
 
-<button on:click={startVideo}>Start videos</button>
-
-<div hidden={isHidden} id="pixiElement"></div>
-
-<div hidden={isHidden}>
 	<button
 		on:click={() => {
-			const newVideoObject = getVideoObjectByName('Hub');
-			setCurrentVideo(newVideoObject);
-
-			playCurrentVideo();
-		}}>Hub</button
+			const player = videoPlayers[currentVideoIndex];
+			const element = videoElements[currentVideoIndex];
+			player.play();
+			element.style.display = 'block';
+			console.log('Play Video clicked');
+		}}>Play Video</button
 	>
 
 	<button
 		on:click={() => {
-			const newVideoObject = getVideoObjectByName('Wheel');
-			setCurrentVideo(newVideoObject);
-
-			playCurrentVideo();
-		}}>Wheel</button
+			switchVideo();
+			console.log('Next Video clicked');
+		}}>Next Video</button
 	>
-
-	<button
-		on:click={() => {
-			const newVideoObject = getVideoObjectByName('Painting');
-			setCurrentVideo(newVideoObject);
-
-			playCurrentVideo();
-		}}>Painting</button
-	>
-
-	<button
-		on:click={() => {
-			const newVideoObject = getVideoObjectByName('Lamp');
-			setCurrentVideo(newVideoObject);
-
-			playCurrentVideo();
-		}}>Lamp</button
-	>
+	<button on:click={() => updateTexture()}>Draw video</button>
 </div>
 
-<PixiSound />
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.video-container {
+		position: relative;
+		width: 800px;
+		height: 450px;
+	}
+</style>
